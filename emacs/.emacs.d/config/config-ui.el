6740b96f6eb634f8d62fa5102288b6cb110a4530
Adam McClenaghan <adam@mcclenaghan.co.uk>
2025-04-21 22:02:40 +0100

    remove smoother scrolling

--- emacs/.emacs.d/config/config-ui.el
+++ emacs/.emacs.d/config/config-ui.el
@@ -1,11 +1,3 @@
-;; This makes scroll a bit nicer, but it can be annoying... not decided whether to keep it or not yet
-(setq scroll-margin 3
-      scroll-conservatively 101
-      scroll-up-aggressively 0.01
-      scroll-down-aggressively 0.01
-      scroll-preserve-screen-position t
-      auto-window-vscroll nil)
-
 (setq create-lockfiles nil)
 
 (unless (file-exists-p "~/.saves")
